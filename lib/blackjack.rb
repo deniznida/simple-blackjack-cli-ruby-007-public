48792f967803a125da56d4973948fbe6996b4651
deniznida <denizunat@hotmail.com>
2015-02-06 09:12:35 -0500

    fixed blackjack code, now ironboard specs are passing

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -17,7 +17,6 @@ end
 def prompt_user
   # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
-  input = gets.strip
 end
 
 def initial_round
@@ -33,7 +32,7 @@ end
 
 def get_user_input
   # code #get_user_input here
-  gets.chomp
+  gets.strip.chomp
 end
 
 def hit? (card_total)
